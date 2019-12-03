90b5cf5b80239ef8b2020f4243e77a9f8ce72e38
siddharth-reddy <34097347+siddharth-reddy@users.noreply.github.com>
2019-12-03 14:48:20 +0530

    Update q1.s

--- q1.s
+++ q1.s
@@ -10,25 +10,25 @@ __main  FUNCTION
 		;EOR R0, R0, R1; R0 XOR R1
 		;MOV R0, #1		;POWER OF EXPONENTIAL
 		;MOV R1, #10	;NUMBER OF TERMS TAKEN FOR SUMMATION
-		VLDR.F32 S0, =14.0	;i
-		VLDR.F32 S1, =10.0	;j
-		MOV R1, #10
+		VLDR.F32 S0, =14.0	;i power of exponential(x)
+		VLDR.F32 S1, =10.0	;j number of terms in the series
+		MOV R1, #10		; number of times it will loop
 		VLDR.F32 S3, =0.0	;COUNTER K
-		MOV R3, #0
-		VLDR.F32 S4, =0.0	;SUM
-		VLDR.F32 S5, =1.0	;DEN
+		MOV R3, #0		;counter
+		VLDR.F32 S4, =0.0	;******SUM(output e^x)*********
+		VLDR.F32 S5, =1.0	;DENom
 		VLDR.F32 S6, =1.0	;ii
 		VLDR.F32 S7, =0.0 	;ii/DEN
 		VLDR.F32 S8, =1.0	;#1
 		
 	
-COMP    CMP R3,R1
+COMP    CMP R3,R1			;counter runs r1 times
 		BLT LOOP
 		BEQ stop
 		BGE stop
 
 LOOP	VDIV.F32 S7,S6,S5	;II/DEN
-		VADD.F32 S4,S4,S7	;SUM
+		VADD.F32 S4,S4,S7	;SUM*****(outpute^x)*****
 		VMUL.F32 S6,S6,S0	;II	
 		VADD.F32 S3,S3,S8	;
 		VMUL.F32 S5,S5,S3	;DEN
